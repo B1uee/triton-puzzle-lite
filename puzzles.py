64a6694495160e3c0ba4fec0dd076a67c075ccf1
B1uee <379264406@qq.com>
2025-07-01 00:45:07 +0800

    finish puzzle 9

--- puzzles.py
+++ puzzles.py
@@ -595,7 +595,7 @@ def flashatt_spec(
     soft = x_exp / x_exp.sum(1, keepdim=True)
     return (v[None, :] * soft).sum(1)
 
-
+# 标量版本的fa
 @triton.jit
 def flashatt_kernel(
     q_ptr, k_ptr, v_ptr, z_ptr, N0, T, B0: tl.constexpr, B1: tl.constexpr
@@ -604,7 +604,43 @@ def flashatt_kernel(
     log2_e = 1.44269504
     myexp = lambda x: tl.exp2(log2_e * x)
     # Finish me!
+    off_i = block_id_i * B0 + tl.arange(0, B0)
+    mask_i = off_i < N0
+    inf = 1.0e6
+
+    # Need `other`!!!
+    q = tl.load(q_ptr + off_i, mask=mask_i)
+    exp_sum = tl.zeros((B0,), dtype=tl.float32)
+    qk_max = tl.full((B0,), -inf, dtype=tl.float32)
+    z = tl.zeros((B0,), dtype=tl.float32) # z其实只代表分子了
+
+    for id_j in tl.range(0, T, B1):
+        off_j = id_j + tl.arange(0, B1)
+        mask_j = off_j < T
+        mask_ij = mask_i[:, None] & mask_j[None, :]
+
+        k = tl.load(k_ptr + off_j, mask=mask_j)
+        qk = q[:, None] * k[None, :] + tl.where(mask_ij, 0, -1.0e6)
+        # print(qk.shape)
+
+        new_max = tl.maximum(tl.max(qk, axis=1), qk_max)
+        qk_exp = tl.exp2(log2_e * (qk - new_max[:, None]))
+        factor = tl.exp2(log2_e * (qk_max - new_max))
+        new_exp_sum = exp_sum * factor + tl.sum(qk_exp, axis=1)
+        # 以上部分与softmax求分母的部分相同
+        # 外层求和的j与分母无关，只与分子的k_j和v_j有关
+ 
+        # 把分子分母分开累加，但是每次缩放和累加都保证同步，最后再一起归一化，结果也是对的
+        v = tl.load(v_ptr + off_j, mask=mask_j, other=0.0)
+        z = z * factor + tl.sum(qk_exp * v[None, :], axis=1) # 分子也是这个公式
+
+        qk_max = new_max
+        exp_sum = new_exp_sum
+
+    z = z / exp_sum
+    tl.store(z_ptr + off_i, z, mask=mask_i)
     return
+
 
 
 r"""
